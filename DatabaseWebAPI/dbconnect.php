4aae3c34409024c287a40a2c873ee576a6a26f73
mikeygrif <mklgrif@gmail.com>
2016-09-01 15:56:19 +0200

    updated to be able to connect to OVH database

--- DatabaseWebAPI/dbconnect.php
+++ DatabaseWebAPI/dbconnect.php
@@ -1,5 +1,5 @@
 <?php 
-$mysqli = new mysqli("localhost", "filou", "zdMPwQicwALVoJt6", "filoustics");
+$mysqli = new mysqli("db624165429.db.1and1.com", "dbo624165429", "huPafsIXRdfFeTkhCNdZ", "db624165429");
 if ($mysqli->connect_errno) {
     echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
